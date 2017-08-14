d674614cbe19c10a553e56d8e12d2771639805e0
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2017-08-14 09:01:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById("main-text");
 element.innerHTML = "new value";
 
 //move the image
-var img = document.getElementById("sammy");
+var img = document.getElementById('sammy');
 img.onclick = function(){
     img.style.marginleft = '100px';
 };
