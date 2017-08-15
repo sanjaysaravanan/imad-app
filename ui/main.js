905a521f45f0b70b2fa4807540cc353f71facaa7
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2017-08-15 17:03:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,4 +23,21 @@ button.onclick = function(){
     //make the request 
     request.open('GET','http://sanjaysaravanan38.imad.hasura-app.io/counter', true);
     request.send(null);
-};    
+};   
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name  = nameInput.value;
+var submit = document.getElementById('submit_button');
+submit.onclick = function() {
+  //make a request to the server and send the name
+  
+  //Capture a list of names and render it as a list
+  var name = ['name1', 'name2', 'name3','name4'];
+  var list = '';
+  for (var i = 0;i<names.length;i++){
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
