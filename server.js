3ef8fd9e2c251c37e537771cf8f200c6715dfe3b
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2017-08-30 16:59:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,8 @@ app.get('/articles/:articleName',function(req,res){
     //this will make articleName == articleOne
     //this will make articlesName == {} contents of article one
     
-    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err,result){
+    //SELECT * FROM article WHERE title = article-one
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }else{
