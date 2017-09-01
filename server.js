e37d88354a46cce056a93bd42e2dd74c7d814415
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2017-09-01 21:14:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,11 +81,35 @@ app.post('/create-user', function(req,res){
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)',[username, dbString], function(req, res){
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)',[username, dbString], function(err, result){
         if (err){
             res.status(500).send(err.toString());
         }else {
             res.send("User successfully created:" + username);
+        }
+   });
+});
+
+app.post('/login', function(req,res){
+   var username = req.body.username;
+   var password = req.body.password;   var dbString = hash(password, salt);
+   pool.query('SELECT * from "user" username = $1',[username], function(err, result){
+        if (err){
+            res.status(500).send(err.toString());
+        }else {
+            if(result.rows.length === 0){
+                res.send(403).send('username/password is invalid');
+            }else{
+                // Match the password
+                var dbString = result.rows[0].password;
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(password, salt());//Creating a hash based on the password submitted and the original salt
+                if (hashedPassword === dbString){
+                res.send('Credentials are correct');
+                } else {
+                    res.send(403).send('username/password is invalid');
+                }
+            }
         }
    });
 });
